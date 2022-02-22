13f7dc1c9183cd1fe62a80615ffdf46fc6794abf
KingClub7 <96010932+KingClub7@users.noreply.github.com>
2022-02-22 15:21:41 +0900

    dethefect

---
+++ SPIC_GAME/Assets/C#/DethEfect.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DethEfect : MonoBehaviour
+{
+    public GameObject[] obj = new GameObject[4];
+    public int boxMax;
+    private Rigidbody[] rigi = new Rigidbody[4];
+    private float rnd1;
+    private float rnd2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            for(int i = 0; i < boxMax; i++)
+            {
+                rnd1 = Random.Range(-0.2f, 0.2f);
+                rnd2 = Random.Range(-0.2f, 0.2f);
+                Instantiate(obj[i], transform.TransformPoint(rnd1, 0, rnd2)
+                    , Quaternion.Euler(0, 0,90*i));
+                //rigi[i] = obj[i].GetComponent<Rigidbody>();
+                //rigi[i].AddForce(transform.forward * 0.1f, ForceMode.Force);
+                Debug.Log(i);
+            }
+            
+        }
+    }
+}
---
+++ SPIC_GAME/Assets/C#/DethEfectApple.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DethEfectApple : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public GameObject OBJ;
+    private float EfeTime;
+    private float rnda;
+    private float rndb;
+    private float rndc;
+    private bool EfeStart;
+    private int EfeTimeCount;
+    private int EfeTimeMax = 10;
+    public static int EfeTimer = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        EfeTime = 0;
+        EfeTimeCount = 0;
+        EfeStart = false;
+        Destroy(gameObject, 4.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EfeStart)
+        {
+            EfeStart = true;
+        }
+
+        if (EfeStart) EFECT();
+        if (EfeTimeCount >= EfeTimeMax * EfeTimer)
+        {
+            EfeStart = false;
+            EfeTimeCount = 0;
+        }
+    }
+    private void EFECT()
+    {
+        if (EfeTime >= 0.001f)
+        {
+            rnda = Random.Range(-0.6f, 0.6f);
+            rndb = Random.Range(-0.6f, 0.6f);
+            rndc = Random.Range(-0.6f, 0.6f);
+            OBJ = Instantiate(boxPrefab, transform.TransformPoint(rnda, rndb, rndc), Quaternion.identity);
+            //OBJ.transform.parent = transform;
+            EfeTime = 0;
+            EfeTimeCount++;
+            Destroy(OBJ, 1.0f);
+        }
+        EfeTime += Time.deltaTime;
+    }
+}
