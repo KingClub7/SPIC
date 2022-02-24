2e5cf4edf585e9c4923082badbbe47895d4afa14
murakamiha <99775983+murakamiha@users.noreply.github.com>
2022-02-24 15:26:25 +0900

    敵２

---
+++ SPIC_GAME/Assets/C#/Enemy2.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2 : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 move = new Vector3(-2f, 0, 0);
+    [SerializeField]
+    private float time = 1f;
+
+    private Vector3 originalPosition;
+    private float theta;
+    [SerializeField]
+    private bool floorMove;
+    private bool floorRotate;
+
+    [SerializeField]
+    private Transform movefloorT;
+
+    //[SerializeField]
+    //private GameObject buttonRotate;
+
+    private float rotateTime;
+
+
+
+    [SerializeField]
+    private Collider collide;
+
+    //[SerializeField]
+    //private BoxCollider hitCollider;//当たり判定
+
+    //[SerializeField]
+    //private BoxCollider desCollider;//死亡判定
+
+    [SerializeField]
+    private float hittime = 5;//
+
+    private float cuntHitTime;
+
+    private Quaternion rot;
+
+    [SerializeField]
+    private GameObject kutibasi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPosition = transform.position;
+        cuntHitTime = 0.0f;
+        Quaternion rot = kutibasi.transform.rotation;
+        floorMove = true;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+            if (floorMove)
+            {
+                //指定時間で1周する計算
+                if (time != 0)
+                {
+                    theta += Mathf.PI * 2.0f * (1.0f / time) * Time.deltaTime;
+                }
+                //取得
+                float c = Mathf.Cos(theta);
+                //変換
+                c = (1.0f - c) * 0.5f;
+                //元から目的地まで往復する座標算出
+                Vector3 position = Vector3.Lerp(originalPosition, originalPosition + move, c);
+                transform.position = position;
+                //if (transform.position == originalPosition + move || transform.position == originalPosition)
+                //{
+                //    floorMove = false;
+                //    floorRotate = true;
+                //}
+            }
+
+        //if (floorRotate)
+        //{
+        //    //回転
+        //    if (rotateTime < 0.5f)
+        //    {
+        //        movefloorT.Rotate(0, 360 * Time.deltaTime, 0);
+        //    }
+        //    rotateTime += Time.deltaTime;
+        //}
+        //if (rotateTime >= 0.5f)
+        //{
+        //    floorMove = true;
+        //    floorRotate = false;
+        //    rotateTime = 0;
+        //}
+
+        if (cuntHitTime >= hittime )
+        {
+            cuntHitTime = 0.0f;
+            Vector3 pos = this.transform.position;
+            Instantiate(kutibasi, pos, Quaternion.identity);
+        }
+        cuntHitTime += Time.deltaTime;
+
+
+        //if (Time.frameCount%hittime==0)
+        //{
+        //    Vector3 pos = this.transform.position;
+        //    Instantiate(kutibasi, pos,Quaternion.identity);
+            
+        //}
+        //cuntHitTime += Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //衝突
+        Player player = other.GetComponent<Player>();
+        if (player != null)
+        {
+            //collide.enabled = false;
+
+            Destroy(gameObject);
+            floorMove = false;
+            floorRotate = false;
+
+        }
+    }
+}
---
+++ SPIC_GAME/Assets/C#/kutibasi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kutibasi : MonoBehaviour
+{
+    //private BoxCollider controller;
+    [SerializeField]
+    private float maxFallSpeed = 10.0f;//最大落下速度
+
+    [SerializeField]
+    private float gravity = 60.0f;//重力
+
+    private float verticalSpeed; //垂直移動度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateGravity();
+        UpdateMovement();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Enemy2 enemy2 = other.GetComponent<Enemy2>();
+        //if(enemy2==null)
+        Destroy(gameObject);
+    }
+
+
+
+    private void UpdateGravity()
+    {
+
+        
+        verticalSpeed -= gravity * Time.deltaTime;
+        
+        //垂直移動速度が最大落下速度を超えないように制限
+        verticalSpeed = Mathf.Max(verticalSpeed, -maxFallSpeed);
+    }
+
+    private void UpdateMovement()
+    {
+
+
+        //移動量を計算
+        Vector3 move = new Vector3(0, verticalSpeed, 0);
+
+
+
+        //移動処理(この関数内でOnControllerColliderHit関数が呼ばれる)
+        this.transform.position+=move * Time.deltaTime;
+
+
+    }
+}
