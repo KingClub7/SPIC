0dcc406ca8a250f1309932e5fa909d450a9a9f05
murakamiha <99775983+murakamiha@users.noreply.github.com>
2022-02-25 10:15:00 +0900

    回転ブロック

--- SPIC_GAME/Assets/C#/RotateBox1.cs
+++ SPIC_GAME/Assets/C#/RotateBox1.cs
@@ -40,17 +40,25 @@ public class RotateBox1 : MonoBehaviour
 
     private bool floorR=false;
 
+    [SerializeField]
+    private float speed = 120;
+
     private Player player;
 
+    private bool upRotateBox;
+
+    private int cuntr;
     //[SerializeField]
     //private Material emptyBoxMaterial;//マテリアル
 
     // Start is called before the first frame update
     void Start()
     {
+        Boxtime =0;
         position = originalPosition = transform.position;
-
+        cuntr = 1;
     }
+    
 
     // Update is called once per frame
     void Update()
@@ -60,44 +68,66 @@ public class RotateBox1 : MonoBehaviour
 
         if (floorR)
         {
-            if (Boxtime < 1.0f)
-            {
-                oyaPos.Rotate(0, 0, 180 * Time.deltaTime);
-            }
             Boxtime += Time.deltaTime;
+            if (Boxtime < 1.6f)
+            {
+                // oyaPos.Rotate(0, 0, 90f * Time.deltaTime);
+                float sp = speed * Time.deltaTime;
+                oyaPos.rotation = Quaternion.RotateTowards(oyaPos.rotation, Quaternion.Euler(0, 0, -180f * cuntr), sp);
+            }
         }
-       
-        if (Boxtime >= 1.0f)
+
+        if (Boxtime >= 1.6f)
         {
             floorR = false;
             Boxtime = 0;
             player.lnversionbool();
             player = null;
+            upRotateBox = false;
+            cuntr++;
         }
     }
     //箱の下側に当たった時の処理
     private void OnTriggerEnter(Collider other)
     {
-        //プレイヤーに衝突した場合
-        player = other.GetComponent<Player>();
-        PlayerRX player2 = other.GetComponent<PlayerRX>();
-        PlayerRX2 player3 = other.GetComponent<PlayerRX2>();
-        PlayerRX3 player4 = other.GetComponent<PlayerRX3>();
-        if (player != null || player2 != null || player3 != null || player4 != null)
+        if (upRotateBox == false)
         {
-            
-
-
-
-
-            if (oyaPos != null)
+            //プレイヤーに衝突した場合
+            player = other.GetComponent<Player>();
+            PlayerRX player2 = other.GetComponent<PlayerRX>();
+            PlayerRX2 player3 = other.GetComponent<PlayerRX2>();
+            PlayerRX3 player4 = other.GetComponent<PlayerRX3>();
+            if (player != null || player2 != null || player3 != null || player4 != null)
             {
-                floorR = true;
-                player.lnversionbool();
+
+
+
+
+
+                if (oyaPos != null)
+                {
+                    floorR = true;
+                    foreach (BoxCollider body in this.GetComponentsInChildren<BoxCollider>())
+                    {
+
+                        if (body)
+                        {
+                            if (body.isTrigger)
+                            {
+                                body.center = -body.center;
+                            }
+                        }
+
+                    }
+
+                    player.lnversionbool();
+
+                    upRotateBox = true;
+
+                }
+
 
             }
-
-
         }
     }
 
